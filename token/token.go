0e65d0e41d4885ff50c1bbdb5cebf6aeb4e92e3a
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2022-11-02 13:30:48 +0800

    add keywords

--- token/token.go
+++ token/token.go
@@ -1,13 +1,18 @@
 package token
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // LookupIdent checks the keywords table to see whether the given identifier
 // is in fact a keyword. If it is, it returns the keyword's TokenType constant.
-// If it isn't, we just get back token.IDENT, which is the TokenType for all 
+// If it isn't, we just get back token.IDENT, which is the TokenType for all
 // user-defined identifiers.
 func LookupIdent(ident string) TokenType {
 	if tok, ok := keywords[ident]; ok {
@@ -16,9 +21,9 @@ func LookupIdent(ident string) TokenType {
 	return IDENT
 }
 
-// TokenType is defined as a string type. That allows us to use many different 
+// TokenType is defined as a string type. That allows us to use many different
 // values as TokenTypes, which in turn allows us to disinguish between different
-// types of tokens. Using string also has the advantage of being easy to debug 
+// types of tokens. Using string also has the advantage of being easy to debug
 // without a lot of boilerplate and helper functions: we can just print a string.
 type TokenType string
 
@@ -36,12 +41,12 @@ const (
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
-	MINUS = "-"
-	BANG = "!"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
 	ASTERISK = "*"
-	SLASH = "/"
+	SLASH    = "/"
 
 	LT = "<"
 	GT = ">"
@@ -58,4 +63,9 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
