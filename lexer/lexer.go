bb9ea499e894d5f4b74d2f8c5d98e68c6e0c72ba
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2022-11-02 13:37:45 +0800

    add == and != and register them

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,7 +32,13 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -46,7 +52,13 @@ func (l *Lexer) NextToken() token.Token {
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -71,7 +83,7 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Type = token.INT
 			tok.Literal = l.readNumber()
 			return tok
-		}else {
+		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
 	}
@@ -98,7 +110,18 @@ func (l *Lexer) skipWhitespace() {
 	}
 }
 
-// readNumber method is exactly the same as readIndentifier except for its 
+// peekChar is really similar to readChar, except that it doesn't increment
+// l.position and l.readPosition. We only want to "peek" ahead in the input
+// and not move around in it, so we know what a call to readChar would return.
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+// readNumber method is exactly the same as readIndentifier except for its
 // usage of isDigit instead of isLetter.
 func (l *Lexer) readNumber() string {
 	position := l.position
--- token/token.go
+++ token/token.go
@@ -51,6 +51,9 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
